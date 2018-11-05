c503fb68478f71335a7b0e0fcf46c19211334073
biancacharlotin <biancacharlotin@gmail.com>
2018-11-05 17:15:48 +0000

    automatically backed up by learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -39,9 +39,10 @@ end
 end
 
 def until_iterator(number_of_times)
-  
-  
-  
+  counter = 0 
+  until counter== number_of_times
+  counter= counter + 1 
+  puts "Welcome to Flatiron School's Web Development Course!"
   # code your solution here using the "until" keyword
   # hint: use a counter to tell the until loop when to stop!
   phrase = "Welcome to Flatiron School's Web Development Course!"
