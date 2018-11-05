082837be45a372b1195a43da7345adb692f8be0a
biancacharlotin <biancacharlotin@gmail.com>
2018-11-05 16:57:08 +0000

    automatically backed up by learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -3,7 +3,7 @@
 def loop_iterator(number_of_times)
   counter = 0
   loop do
-      counter = counter + 1
+    counter = counter + 1
     puts "Welcome to Flatiron School's Web Development Course!"
   
   break if counter == number_of_times 
